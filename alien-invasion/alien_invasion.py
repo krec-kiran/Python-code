41b7685710d11c51d4630164428e3d86f85680a0
krec-kiran <krec.kiran@gmail.com>
2017-11-14 10:23:53 +0000

    Gaming Project

---
+++ alien-invasion/alien_invasion.py
@@ -0,0 +1,33 @@
+import sys
+import pygame
+from ship import Ship
+
+
+class Settings():
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+
+    ship = Ship(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        pygame.display.flip()
+
+run_game()
