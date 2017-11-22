2310ffd793544044e17417fc3ff86fa295d51e6e
krec-kiran <krec.kiran@gmail.com>
2017-11-22 14:58:57 +0000

    multi-list-array

---
+++ multi_list.py
@@ -0,0 +1,16 @@
+input_str = input()
+dimensions = [int(x) for x in input_str.split(',')]
+print(dimensions)
+rowNum = dimensions[0]
+colNum = dimensions[1]
+
+multilist = [[0 for col in range(colNum)] for row in range(rowNum)]
+
+print(multilist)
+
+for row in range(rowNum):
+    for col in range(colNum):
+        multilist[row][col] = row * col
+
+
+print(multilist)
---
+++ test-workspace.py
@@ -0,0 +1,6 @@
+num=10
+
+num+=50
+
+num+=1
+print(num)
