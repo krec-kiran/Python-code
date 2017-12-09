2611cd468f23be7e7465c6f8e6bf4faf2e54f6d4
krec-kiran <krec.kiran@gmail.com>
2017-12-09 21:47:34 +0000

    code

---
+++ equal-sides.py
@@ -0,0 +1,20 @@
+'''You are going to be given an array of integers. Your job is to take that array and find an index N
+where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
+If there is no index that would make this happen, return -1.'''
+
+
+a = [20,10,-80,10,10,15,35]
+
+def equal_check(a):
+  for i in range(len(a)):
+    right_side = a[i + 1:]
+    right_sum = sum(right_side)
+    left_side = a[0:i]
+    left_sum = sum(left_side)
+    if sum(left_side) == sum(right_side):
+      return i
+
+  return -1
+
+print(equal_check(a))
+
