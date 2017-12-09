1126af48e41b3e4b05126e1293017ef26059a241
krec-kiran <krec.kiran@gmail.com>
2017-12-09 23:30:42 +0000

    code

---
+++ format_string.py
@@ -0,0 +1,29 @@
+# namelist= [ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ]
+# namelist=[ {'name': 'Bart'} ]
+# namelist=[]
+namelist=[{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'},{'name': 'Homer'},{'name': 'Marge'}]
+
+names = [li['name'] for li in namelist ]
+
+def namelisting(namelist):
+  s=''
+  names = [li['name'] for li in namelist ]
+  if len(names) > 1:
+    s="'",", ".join(names[:-1])," & "," ".join(names[-1:]),"'"
+    return ''.join(s)
+  elif len(names)==1:
+    s="'","".join(names),"'"
+    return ''.join(s)
+  else:
+    return s
+
+
+print(namelisting(namelist))
+
+if len(names) > 1:
+  print("'",", ".join(names[:-1]),"and","".join(names[-1:]),"'")
+else:
+  print("'","".join(names),"'")
+
+
+
