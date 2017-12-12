a3a48b965930c0ba475db3e3ce1a2861fdb5d1db
krec-kiran <krec.kiran@gmail.com>
2017-12-12 20:52:28 +0000

    code

---
+++ count_digit.py
@@ -0,0 +1,15 @@
+'''Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+Square all numbers k (0 <= k <= n) between 0 and n. Count the numbers of digits d used in
+the writing of all the k**2. Call nb_dig (or nbDig or ...)
+the function taking n and d as parameters and returning this count.'''
+
+def nb_dig(n, d):
+  d=str(d)
+  i=0
+  l=[x*x for x in range(n+1)]
+  l=str(l)
+  for x in l:
+    if d in x:
+      i+=1
+  return(i)
+
---
+++ mid_char.py
@@ -0,0 +1,20 @@
+text="A"
+
+length=len(text)
+if length%2==0:
+    mid=int(length/2)
+    print(text[mid-1:mid+1])
+else:
+    mid=int((length-1)/2)
+    print(text[mid])
+
+# def mystery(n):
+#     print(n)
+#     if not int(n):
+#       return -1
+
+# def wrap_mystery(n):
+#     return mystery(n)
+
+
+# print(wrap_mystery(2.4))
