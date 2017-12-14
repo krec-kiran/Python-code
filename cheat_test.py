3b83d929b9733e2aabe880e213e5bcdec56236c9
krec-kiran <krec.kiran@gmail.com>
2017-12-14 08:32:13 +0000

    code

--- cheat_test.py
+++ cheat_test.py
@@ -1,3 +1,10 @@
+'''A friend of mine takes a sequence of numbers from 1 to n (where n > 0).
+Within that sequence, he chooses two numbers, a and b.
+He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+Given a number n, could you tell me the numbers he excluded from the sequence?
+
+removNb(26) should return [(15, 21), (21, 15)] '''
+
 import copy
 def removNb(n):
   original_elements=[x for x in range(n)]
