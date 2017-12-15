b8b5e185533561acfe1a4c048d00ea79be21cedc
Kiran <kiran@aryans-imac.home>
2017-12-15 09:17:59 +0000

    code changes

--- cheat_test.py
+++ cheat_test.py
@@ -31,52 +31,52 @@ removNb(26) should return [(15, 21), (21, 15)] '''
 
 #   return(result)
 
-# def removNb(n):
-#   elements=[x for x in range(n)]
-#   elements=elements[2:]
-#   toto=sum(range(n+1))
-#   result=[]
-#   for x in elements:
-#     test_elements=elements[1:]
-#     for y in test_elements:
-#       if x*y==(toto-(x+y)):
-#         tup1=(x,y)
-#         tup2=(y,x)
-#         result.append(tup1)
-#         result.append(tup2)
-#         return(result)
-#   return(result)
-
-
 def removNb(n):
   elements=[x for x in range(n)]
   elements=elements[2:]
   toto=sum(range(n+1))
   result=[]
-  count=0
-  # for x in elements:
-  x=elements[count]
-  count+=1
-  test_elements=elements[1:]
-  print(x,test_elements)
-  for y in test_elements:
-    print(y)
-    if x*y==(toto-(x+y)):
-      tup1=(x,y)
-      tup2=(y,x)
-      result.append(tup1)
-      result.append(tup2)
-      return(result)
-  if(elements is None):
-    return(result)
+  for x in elements:
+    test_elements=elements[1:]
+    for y in test_elements:
+      if x*y==(toto-(x+y)):
+        tup1=(x,y)
+        tup2=(y,x)
+        result.append(tup1)
+        result.append(tup2)
+        return(result)
+  return(result)
+
+
+# def removNb(n):
+#   elements=[x for x in range(n)]
+#   elements=elements[2:]
+#   toto=sum(range(n+1))
+#   result=[]
+#   count=0
+#   # for x in elements:
+#   x=elements[count]
+#   count+=1
+#   test_elements=elements[1:]
+#   print(x,test_elements)
+#   for y in test_elements:
+#     print(y)
+#     if x*y==(toto-(x+y)):
+#       tup1=(x,y)
+#       tup2=(y,x)
+#       result.append(tup1)
+#       result.append(tup2)
+#       return(result)
+#   if(elements is None):
+#     return(result)
 
 
 print(removNb(10))
-# print(removNb(67))
-# print(removNb(197))
-# print(removNb(201))
-# print(removNb(3006))
-# print(removNb(10045))
-# print(removNb(15045))
+print(removNb(67))
+print(removNb(197))
+print(removNb(201))
+print(removNb(3006))
+print(removNb(10045))
+print(removNb(15045))
 
 
