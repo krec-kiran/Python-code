c6612d5ac4091c4664c4c09f3e5a70ee6f7837e4
krec-kiran <krec.kiran@gmail.com>
2018-11-24 23:03:46 +0000

    changes!

--- missing_positive.py
+++ missing_positive.py
@@ -2,12 +2,16 @@ class Solution:
     def findPositive(nums):
         if not nums:
             return
-        small = min([x > 0 for x in nums])
+        small = max([x for x in nums if x < 0])
+        if small != -1:
+            small = min([x for x in nums if x > 0])
         for i in range(len(nums)):
-            if small not in nums and small > 0:
+            if small not in nums:
                 return small
             else:
                 small += 1
 
 
 print(Solution.findPositive([2, 3, 7, 6, 8, -1, -10, 15]))
+print(Solution.findPositive([2, 3, -7, 6, 8, 1, -10, 15]))
+print(Solution.findPositive([1, 1, 0, -1, -2]))
